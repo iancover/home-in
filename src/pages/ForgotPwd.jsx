adbd0a5b3c928bec963a65707aa2e598d8271b84
Ian Cover <iancover@gmail.com>
2022-02-05 12:55:41 -0600

    feat: Add Navbar component with Icons as Components
    - Add Navbar as footer since its a 'mobile first' layout
    - Import icons as React Components for Explore, Offers & Profile
      - nest icon comps under list items and useNavigate to route onClick
      - pathMatchRoute: fn that uses useLocation hook to change icon
        colors if page is active with 'fill={}'

---
+++ src/pages/ForgotPwd.jsx
@@ -0,0 +1,9 @@
+function ForgotPwd() {
+  return (
+    <div>
+      <h1>Forgot Pwd</h1>
+    </div>
+  );
+}
+
+export default ForgotPwd;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
+
+export default SignIn;
