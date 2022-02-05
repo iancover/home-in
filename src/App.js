efe4177e72e5fd495eeb00854a732f2f4dd4d80d
Ian Cover <iancover@gmail.com>
2022-02-05 12:49:28 -0600

    feat: Add routes with React Router
    - Install 'react-router-dom'
    - Import pages: Explore, Offers, Profile, SignIn, SignUp, ForgotPwd
    - For '/profile' route send to 'SignUp' for now
    - Import Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPwd from './pages/ForgotPwd';
+
 function App() {
   return (
     <>
-      <h1 className='home-in'>Home In</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-pwd' element={<ForgotPwd />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 }
