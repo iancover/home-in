f5857284fbec9e2dac95f966b9082977fbf484de
Ian Cover <iancover@gmail.com>
2022-04-10 03:11:41 -0500

    feat: Add Explore page
    Simple Explore page with temporary links to SignIn & SignUp

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Explore() {
+
   return (
     <div>
       <h1>Explore</h1>
+      <Link to='/sign-in'>
+        Sign In
+      </Link>
+      <Link to='/sign-up'>
+        Sign Up
+      </Link>
     </div>
   );
 }
