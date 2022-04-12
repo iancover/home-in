79affd7cebdf6c193e1d81d4532b728006f87c61
Ian Cover <iancover@gmail.com>
2022-04-12 13:52:37 -0500

    feat: Add Spinner component and update comments

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,19 +1,28 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStatus } from '../hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 const PrivateRoute = () => {
   const { loggedIn, checkStatus } = useAuthStatus();
-  
-  if (checkStatus) {
-    return <h3>Loading...</h3>
-  }
 
+  if (checkStatus) {
+    return <Spinner />;
+  }
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
 };
 
 export default PrivateRoute;
 
-// 'useAuthStatus' checks if auth user before redirecting to SignIn
+// NOTES
+  // Since we cant add a <PrivateRoute> component directly w React Router v6
+  // to redirect an auth user to their profile instead of to sing in
+  // we create and add the custom hook 'useAuthStatus()', which uses a
+  // Firebase function to handle authentication when changing state.
 
-// if logged in <Outlet /> == <Profile />
-// else go to SignIn pg
+  // So then, if logged in <Outlet> points to <Profile>, the nested route
+  // or else <Navigate to='/sign-in' /> redirects the user to <SignIn>
+  //
+  // <Route path='/profile' element={<PrivateRoute />}>
+  //   <Route path='/profile' element={<Profile />} />
+  // </Route>
+  // <Route path='/sign-in' element={<SignIn />} />
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className='loading-spinner-container'>
+      <div className='loading-spinner'></div>
+    </div>
+  );
+}
+
+export default Spinner;
