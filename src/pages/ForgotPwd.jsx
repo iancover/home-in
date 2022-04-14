3fb14cf6185532032f9166030b5177aed01a7655
Ian Cover <iancover@gmail.com>
2022-04-14 02:31:44 -0500

    feat: Update ForgotPwd component
    - onChange: to display text as type in state
    - onSubmit: send reset email with Firebase and go to sign in

--- src/pages/ForgotPwd.jsx
+++ src/pages/ForgotPwd.jsx
@@ -1,7 +1,57 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+// Firebase & Toastify
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+// Icons
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+
 function ForgotPwd() {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const onChange = (e) => setEmail(e.target.value);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success('Email was sent');
+      navigate('/sign-in');
+    } catch (error) {
+      toast.error('Could not send reset email');
+    }
+  };
+
   return (
-    <div>
-      <h1>Forgot Pwd</h1>
+    <div className='pg-container'>
+      <header>
+        <p className='pg-header'>Forgot Password</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type='email'
+            className='email-input'
+            id='email'
+            value={email}
+            placeholder='Email'
+            onChange={onChange}
+          />
+          <Link className='forgot-pwd-link' to='/sign-in'>
+            Sign In
+          </Link>
+
+          <div className='sign-in-bar'>
+            <div className='sign-in-txt'>Send Reset Link</div>
+            <button className='sign-in-btn'>
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
