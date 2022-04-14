fefa961a8c5e4573510ac46e17698a946d85f19d
Ian Cover <iancover@gmail.com>
2022-04-14 03:50:23 -0500

    feat: Update Explore page add rent & sale listings links

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,16 +1,37 @@
 import { Link } from 'react-router-dom';
+// Images
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
 
 function Explore() {
-
   return (
-    <div>
-      <h1>Explore</h1>
-      <Link to='/sign-in'>
-        Sign In
-      </Link>
-      <Link to='/sign-up'>
-        Sign Up
-      </Link>
+    <div className='explore'>
+      <header>
+        <p className='pg-header'>Explore</p>
+      </header>
+
+      <main>
+        {/* Slider */}
+        <p className='explore-cat-heading'>Categories</p>
+        <div className='explore-cats'>
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              alt='rent'
+              className='explore-cat-img'
+            />
+            <p className='explore-cat-name'>Places for Rent</p>
+          </Link>
+          <Link to='/category/sale'>
+            <img
+              src={sellCategoryImage}
+              alt='sale'
+              className='explore-cat-img'
+            />
+            <p className='explore-cat-name'>Places for Sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
