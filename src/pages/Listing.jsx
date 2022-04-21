c8b6cfc8847fba4221ee8d6fba99bed73d34fa07
Ian Cover <iancover@gmail.com>
2022-04-21 09:36:35 -0500

    feat: Add Listing page which gets & displays data from Firebase
    useEffect
    - Get doc ref from Firebase db collection 'listings' using listing
      id passed in params. ':listingId'
    - If there is a doc, update it data in state and handle loader
    
    The rest will display the data and also include a button to click
    and get the listing URL to share it, and a 'contact landlord' button
    which uses the 'userRef' which links the author of listing.

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+// Firebase
+import { doc, getDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { db } from '../firebase.config';
+// Components
+import Spinner from '../components/Spinner';
+// Icons
+import shareIcon from '../assets/svg/shareIcon.svg';
+
+function Listing() {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+  const params = useParams();
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  // get data
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listings', params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [navigate, params.listingId]);
+
+  // display loader
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <main>
+      {/* SLIDER */}
+      <div
+        className='share-icon-div'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}>
+        <img src={shareIcon} alt='copy link icon' />
+      </div>
+
+      {shareLinkCopied && <p className='link-copied'>Link Copied!</p>}
+
+      <div className='listing-details'>
+        <p className='listing-name'>
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listing-location'>{listing.location}</p>
+        <p className='listing-type'>For {listing.type === 'rent' ? 'Rent' : 'Sale'}</p>
+
+        {listing.offer && (
+          <p className='discount-price'>
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className='listing-details-list'>
+          <li>{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}</li>
+          <li>{listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}</li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        <p className='listing-location-title'>Location</p>
+
+        {/* MAP */}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className='btn-primary'>
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
+  );
+}
+
+export default Listing;
+
+// NOTES:
+// replace() to add comma in thousands $1,000, regex:
+//    \B - not word boundary
+//    (?=(\d{3})) - before three digits
+//    +(?!\d) - and not before a single digit
