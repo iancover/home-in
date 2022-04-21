c49b724188a1de95e44564c264f632f7012f488c
Ian Cover <iancover@gmail.com>
2022-04-21 05:15:09 -0500

    feat: Add Listing route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPwd from './pages/ForgotPwd';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-pwd' element={<ForgotPwd />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
         </Routes>
         <Navbar />
       </Router>
