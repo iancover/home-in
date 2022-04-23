87ecc370e45063852e084f70b03f717ed688743a
Ian Cover <iancover@gmail.com>
2022-04-23 14:32:49 -0500

    refactor: Use regex to display city and state location only

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -4,55 +4,47 @@ import bedIcon from '../assets/svg/bedIcon.svg';
 import bathtubIcon from '../assets/svg/bathtubIcon.svg';
 
 function ListingItem({ listing, id, onDelete }) {
-  // NOTE:
-  //  onEdit & onDelete: option in <Profile> only for current auth user
   return (
-    <li className='cat-listing'>
-      <Link
-        to={`/category/${listing.type}/${id}`}
-        className='cat-listing-link'>
-        <img
-          src={listing.imageUrls[0]}
-          alt={listing.name}
-          className='cat-listing-img'
-        />
-        <div className='cat-listing-details'>
-          <p className='cat-listing-location'>{listing.location}</p>
-          <p className='cat-listing-name'>{listing.name}</p>
-          <p className='cat-listing-price'>
-            ${' '}
-            {listing.offer
-              ? listing.discountedPrice
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-              : listing.regularPrice
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            {listing.type === 'rent' && ' / Month'}
-          </p>
-          <div className='cat-listing-info-div'>
-            <img src={bedIcon} alt='bed' />
-            <p className='cat-listing-info-txt'>
-              {listing.bedrooms > 1
-                ? `${listing.bedrooms} Bedrooms`
-                : '1 Bedroom'}
-            </p>
-            <img src={bathtubIcon} alt='bath' />
-            <p className='cat-listing-info-txt'>
-              {listing.bathrooms > 1
-                ? `${listing.bathrooms} Bathrooms`
-                : '1 Bathroom'}
-            </p>
-          </div>
+    <li>
+      <div className='cat-listing'>
+        <div className='cat-listing'>
+          <Link to={`/category/${listing.type}/${id}`} className='cat-listing-link'>
+            <img src={listing.imageUrls[0]} alt={listing.name} className='cat-listing-img' />
+            <div className='cat-listing-details'>
+              <p className='cat-listing-location'>
+                {listing.location.match(/([A-Z][a-z]+\s?)+,\s[A-Z]{2}/g).toString()}
+              </p>
+              <p className='cat-listing-name'>{listing.name}</p>
+              <p className='cat-listing-price'>
+                ${' '}
+                {listing.offer
+                  ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                  : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                {listing.type === 'rent' && ' / Month'}
+              </p>
+              <div className='cat-listing-info-div'>
+                <img src={bedIcon} alt='bed' />
+                <p className='cat-listing-info-txt'>
+                  {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}
+                </p>
+                <img src={bathtubIcon} alt='bath' />
+                <p className='cat-listing-info-txt'>
+                  {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}
+                </p>
+              </div>
+            </div>
+          </Link>
         </div>
-      </Link>
-      {onDelete && (
-        <DeleteIcon
-          className='remove-icon'
-          fill='rgb(231, 76, 60)'
-          onClick={() => onDelete(listing.id, listing.name)}
-        />
-      )}
+        <div className='flex'>
+          {onDelete && (
+            <DeleteIcon
+              className='delete-icon'
+              fill='rgb(194, 110, 101)'
+              onClick={() => onDelete(listing.id, listing.name)}
+            />
+          )}
+        </div>
+      </div>
     </li>
   );
 }
