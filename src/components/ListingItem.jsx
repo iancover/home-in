93aaf58fe7aaa194135cafb52f658ce49fef0b63
Ian Cover <iancover@gmail.com>
2022-04-25 18:25:52 -0500

    refactor: Shorten code lines printWidth

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -8,28 +8,44 @@ function ListingItem({ listing, id, onDelete }) {
     <li>
       <div className='cat-listing'>
         <div className='cat-listing'>
-          <Link to={`/category/${listing.type}/${id}`} className='cat-listing-link'>
-            <img src={listing.imageUrls[0]} alt={listing.name} className='cat-listing-img' />
+          <Link
+            to={`/category/${listing.type}/${id}`}
+            className='cat-listing-link'>
+            <img
+              src={listing.imageUrls[0]}
+              alt={listing.name}
+              className='cat-listing-img'
+            />
             <div className='cat-listing-details'>
               <p className='cat-listing-location'>
-                {listing.location.match(/([A-Z][a-z]+\s?)+,\s[A-Z]{2}/g).toString()}
+                {listing.location
+                  .match(/([A-Z][a-z]+\s?)+,\s[A-Z]{2}/g)
+                  .toString()}
               </p>
               <p className='cat-listing-name'>{listing.name}</p>
               <p className='cat-listing-price'>
                 ${' '}
                 {listing.offer
-                  ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                  : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                  ? listing.discountedPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                  : listing.regularPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
                 {listing.type === 'rent' && ' / Month'}
               </p>
               <div className='cat-listing-info-div'>
                 <img src={bedIcon} alt='bed' />
                 <p className='cat-listing-info-txt'>
-                  {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}
+                  {listing.bedrooms > 1
+                    ? `${listing.bedrooms} Bedrooms`
+                    : '1 Bedroom'}
                 </p>
                 <img src={bathtubIcon} alt='bath' />
                 <p className='cat-listing-info-txt'>
-                  {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}
+                  {listing.bathrooms > 1
+                    ? `${listing.bathrooms} Bathrooms`
+                    : '1 Bathroom'}
                 </p>
               </div>
             </div>
